1f053dcce64d9028844ee346363fda9d501b25e1
andrey <andrey.g@g.com>
2023-05-11 18:29:40 +0900

    ml++

--- v3d-common-utils/core/v3d_converter.py
+++ v3d-common-utils/core/v3d_converter.py
@@ -1,10 +1,30 @@
-import numpy
+import blosc
+import numpy as np
 
 
-def convert_file_bin2png(path_file: str):
+NAN_MODE_MIN = 0
+NAN_MODE_MAX = 1
+NAN_MODE_MEAN = 2
+NAN_MODE_MEDIAN = 3
+def convert_pcd2png(pcd: np.ndarray, nan_mode: int = NAN_MODE_MIN) -> np.ndarray:
+    depth_data = pcd[..., -1]
+
+    if np.any(np.isnan(depth_data)):
+        if nan_mode == NAN_MODE_MIN: swap_value = np.nanmin(depth_data)
+        elif nan_mode == NAN_MODE_MAX: swap_value = np.nanmax(depth_data)
+        elif nan_mode == NAN_MODE_MEAN: swap_value = np.nanmean(depth_data)
+        elif nan_mode == NAN_MODE_MEDIAN: swap_value = np.nanmedian(depth_data)
+        else: swap_value = 0
+
+        depth_data = np.nan_to_num(depth_data, swap_value)
+
+
+
+
+def convert_file_bin2png(path_file: str, nan_mode: int = NAN_MODE_MIN) -> np.ndarray:
     f = open(path_file, "rb")
     compressed_array = f.read()
     f.close()
+    pcd = blosc.unpack_array(compressed_array)
+    return convert_pcd2png(pcd, nan_mode)
 
-
-    return blosc.unpack_array(compressed_array)
